6350d860248321eb5687396f8c1b8823d7dd638c
lauraparrado <laespaviza@gmail.com>
2022-02-27 16:09:22 -0500

    document the generic function with a docstring

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,8 @@
 def generic_model_mutation_process(model, data, id=None, commit=True):
+    """ Es posible actualizar la instancia de un modelo
+     o crearla. Pero esta es cargada a la base de datos
+     solo si commit es verdadero si no, solo se retorna esta
+     """
     if id:
         item = model.objects.get(id=id)
         try:
