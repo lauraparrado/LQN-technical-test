c17cc70f0e25c369a8f98122ada9eb24de749b8f
lauraparrado <laespaviza@gmail.com>
2022-02-28 01:01:12 -0500

    feature logic exercise 1

---
+++ app/management/commands/logic_exercise_1.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = ''' Desarrolla un algoritmo que imprima los números del 0 al 100. Adicionalmente debe
+    imprimirse en la misma línea la palabra buzz en caso de que el número sea par. Sí el
+    número es múltiplo de 5 debe imprimirse en la misma línea la palabra bazz.
+    '''
+
+    def handle(self, *args, **kwargs):
+        for i in range(101):
+            word = ""
+            if i % 2 == 0:
+                word += " buzz"
+            if i % 5 == 0:
+                word += " bazz"
+
+            self.stdout.write("{}{}".format(i, word))
+
+
